6ebbcad2a098ddd9045e3fc3c3ae6aa8b6f0c991
ZhuoyangM <zhmeng@ucsd.edu>
2022-12-02 13:03:06 -0800

    morefixspace

--- source/test/indexedDB.test.js
+++ source/test/indexedDB.test.js
@@ -56,97 +56,93 @@ test('update job correctly', async () => {
     portalUser: '',
     portalPass: ''
   }
-    
-    await db.setupDB();
-    await db.addJob(mockData);
-    const job = await db.getJob(1);
-    const title = job['title'];
-    expect(title).toEqual('SDE I');
+  await db.setupDB()
+  await db.addJob(mockData)
+  const job = await db.getJob(1)
+  const title = job['title']
+  expect(title).toEqual('SDE I')
 
-    await db.updateJob(mockDataUpdate);
-    const jobUpdate = await db.getJob(1);
-    const titleUpdate = jobUpdate['title'];
-    expect(titleUpdate).toEqual('SDE II');
+  await db.updateJob(mockDataUpdate)
+  const jobUpdate = await db.getJob(1)
+  const titleUpdate = jobUpdate['title']
+  expect(titleUpdate).toEqual('SDE II')
 })
 
 test('get all jobs correctly', async () => {
-    const mockData = {
-        id: 1,
-        company: 'Amazon',
-        title: 'SDE I',
-        description: '',
-        portalURL: '',
-        location: '',
-        status: '',
-        notes: '',
-        contact: '',
-        deadline: '',
-        portalUser: '',
-        portalPass: ''
-    }
+  const mockData = {
+    id: 1,
+    company: 'Amazon',
+    title: 'SDE I',
+    description: '',
+    portalURL: '',
+    location: '',
+    status: '',
+    notes: '',
+    contact: '',
+    deadline: '',
+    portalUser: '',
+    portalPass: ''
+  }
+  const mockDataTwo = {
+    id: 2,
+    company: 'Google',
+    title: 'SDE II',
+    description: '',
+    portalURL: '',
+    location: '',
+    status: '',
+    notes: '',
+    contact: '',
+    deadline: '',
+    portalUser: '',
+    portalPass: ''
+  }
+  await db.setupDB()
+  await db.addJob(mockData)
+  await db.addJob(mockDataTwo)
 
-    const mockDataTwo = {
-        id: 2,
-        company: 'Google',
-        title: 'SDE II',
-        description: '',
-        portalURL: '',
-        location: '',
-        status: '',
-        notes: '',
-        contact: '',
-        deadline: '',
-        portalUser: '',
-        portalPass: ''
-    }
-    await db.setupDB();
-    await db.addJob(mockData);
-    await db.addJob(mockDataTwo);
-
-    const jobs = await db.getAllJobs();
-    expect(jobs.length).toEqual(2);
-    expect(jobs[0]).toEqual(mockData);
-    expect(jobs[1]).toEqual(mockDataTwo);
+  const jobs = await db.getAllJobs()
+  expect(jobs.length).toEqual(2)
+  expect(jobs[0]).toEqual(mockData)
+  expect(jobs[1]).toEqual(mockDataTwo)
 
 })
 
 test('delete job correctly',async () => {
-    const mockData = {
-        id: 1,
-        company: 'Amazon',
-        title: 'SDE I',
-        description: '',
-        portalURL: '',
-        location: '',
-        status: '',
-        notes: '',
-        contact: '',
-        deadline: '',
-        portalUser: '',
-        portalPass: ''
-    }
-
-    const mockDataTwo = {
-        id: 2,
-        company: 'Google',
-        title: 'SDE II',
-        description: '',
-        portalURL: '',
-        location: '',
-        status: '',
-        notes: '',
-        contact: '',
-        deadline: '',
-        portalUser: '',
-        portalPass: ''
-    }
-
-    await db.setupDB();
-    await db.addJob(mockData);
-    await db.addJob(mockDataTwo);
-    await db.deleteJob(1);
-    const jobs = await db.getAllJobs();
-    expect(jobs.length).toEqual(1);
-    expect(jobs[0]).toEqual(mockDataTwo);
+  const mockData = {
+    id: 1,
+    company: 'Amazon',
+    title: 'SDE I',
+    description: '',
+    portalURL: '',
+    location: '',
+    status: '',
+    notes: '',
+    contact: '',
+    deadline: '',
+    portalUser: '',
+    portalPass: ''
+  }
+  const mockDataTwo = {
+    id: 2,
+    company: 'Google',
+    title: 'SDE II',
+    description: '',
+    portalURL: '',
+    location: '',
+    status: '',
+    notes: '',
+    contact: '',
+    deadline: '',
+    portalUser: '',
+    portalPass: ''
+  }
+  await db.setupDB()
+  await db.addJob(mockData)
+  await db.addJob(mockDataTwo)
+  await db.deleteJob(1)
+  const jobs = await db.getAllJobs()
+  expect(jobs.length).toEqual(1)
+  expect(jobs[0]).toEqual(mockDataTwo)
 })
 
