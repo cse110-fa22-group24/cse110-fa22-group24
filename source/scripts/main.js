281a57022a78b9941757b65f3ed7b8b16e948873
PierreBeur <cbeurtheret@ucsd.edu>
2022-12-01 16:17:02 -0800

    Fix display styling and lint

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -80,73 +80,30 @@ function initFormHandler () {
     // Clear the <form> fields
     clearForm()
   })
-  
-  // Add an eventListener for search bar keyup event
+  // Add an eventListener for when a key is released in the search bar
   const searchBar = document.getElementById('search')
-  
-  searchBar.addEventListener('keyup', async (e)=>{
-    //catch the input in search box
-    const keyword = searchBar.value.toLocaleLowerCase().replaceAll(' ', '');
-    const jobElements = document.querySelectorAll('job-details');
-   
-   //loop over all job applications on the page
-    for (const jobEl of jobElements) {
-      const shadowRoot = jobEl.shadowRoot
-
-      // Get all fields that can be searched and also preprocess them
+  searchBar.addEventListener('keyup', () => {
+    // Get the current search query
+    const query = searchBar.value.toLocaleLowerCase().replaceAll(' ', '')
+    // For every job-details element on the page
+    for (const jobDetails of document.querySelectorAll('job-details')) {
+      const shadowRoot = jobDetails.shadowRoot
+      // Get all fields that can be searched and preprocess them
       const company = shadowRoot.querySelector('#company').textContent.toLocaleLowerCase().replaceAll(' ', '')
       const position = shadowRoot.querySelector('#title').textContent.toLocaleLowerCase().replaceAll(' ', '')
       const status = shadowRoot.querySelector('#status').textContent.toLocaleLowerCase().replaceAll(' ', '').slice(7)
       const location = shadowRoot.querySelector('#location').textContent.toLocaleLowerCase().replaceAll(' ', '')
-
-      //if any one of the four fields contains the input, show the item
-      if(company.includes(keyword) || position.includes(keyword)
-         || status.includes(keyword) || location.includes(keyword)){
-        jobEl.style.display = 'flex';
-      }
-      else{//else hide the item
-        jobEl.style.display = 'none';
-      }
-
-    }
-  })
-
-  /** 
-  searchBar.addEventListener('keypress', async (e) => {
-    // if user press enter to search
-    if (e.key === 'Enter') {
-      // Catch and preprocess user input
-      const keyword = searchBar.value.toLocaleLowerCase().replaceAll(' ', '')
-      const jobElements = document.querySelectorAll('job-details')
-
-      // For each job application
-      for (const jobEl of jobElements) {
-        const shadowRoot = jobEl.shadowRoot
-
-        // Get all fields that can be searched and also preprocess them
-        const company = shadowRoot.querySelector('#company').textContent.toLocaleLowerCase().replaceAll(' ', '')
-        const position = shadowRoot.querySelector('#title').textContent.toLocaleLowerCase().replaceAll(' ', '')
-        const status = shadowRoot.querySelector('#status').textContent.toLocaleLowerCase().replaceAll(' ', '').slice(7)
-        const location = shadowRoot.querySelector('#location').textContent.toLocaleLowerCase().replaceAll(' ', '')
-
-        // if there's nothing in the search box, display all job apps by default
-        if (keyword === '') {
-          jobEl.style.display = 'flex'
-        } else {
-          // if none of the fields matches user input, hide the item
-          if (keyword !== company && keyword !== position && keyword !== status && keyword !== location) {
-            jobEl.style.display = 'none'
-          }
-
-          // if one of the fields matches user input, show the item
-          if (keyword === company || keyword === position || keyword === status || keyword === location) {
-            jobEl.style.display = 'flex'
-          }
-        }
+      // If any of the fields match the query
+      if (company.includes(query) || position.includes(query) ||
+           status.includes(query) || location.includes(query)) {
+        // Show the job-details element
+        jobDetails.removeAttribute('style')
+      } else {
+        // Hide the job-details element
+        jobDetails.setAttribute('style', 'display: none')
       }
     }
   })
-  */
 }
 
 /**
