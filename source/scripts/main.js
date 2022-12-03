cdffdf4e1e04dc0b40212090b365d39a54bc1c1e
PierreBeur <cbeurtheret@ucsd.edu>
2022-12-02 19:10:01 -0800

    Add ESLint Changes

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,7 +1,7 @@
 // main.js
 
 // Run the init() function when the page has loaded
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener('DOMContentLoaded', init)
 
 // Starts the program, all function calls trace back here
 function init () {
@@ -9,19 +9,19 @@ function init () {
    * const jobs = database.getAllJobs();
    */
   // Get the jobs from localStorage
-  const jobs = getJobsFromStorage();
+  const jobs = getJobsFromStorage()
   // Add each job to the job-details-list element
-  addJobsToDocument(jobs);
+  addJobsToDocument(jobs)
   // Add the event listeners to the form elements
-  initFormHandler();
+  initFormHandler()
   // Have the new app button show the form when clicked
-  document.querySelector('#new-app-button').addEventListener('click', showForm);
+  document.querySelector('#new-app-button').addEventListener('click', showForm)
   // Add <sort-bar> elements for each sortable field
-  addSortBars(['Company', 'Position', 'Location', 'Status', 'Deadline']);
+  addSortBars(['Company', 'Position', 'Location', 'Status', 'Deadline'])
   // Have the form be hidden to start
-  hideForm();
+  hideForm()
   // Have the delete popup be hidden to start
-  hideDeletePopup();
+  hideDeletePopup()
 }
 
 /**
@@ -32,7 +32,7 @@ function init () {
  * @returns {Array<Object>} An array of jobs found in localStorage
  */
 function getJobsFromStorage () {
-  return JSON.parse(localStorage.getItem('jobs')) || [];
+  return JSON.parse(localStorage.getItem('jobs')) || []
 }
 
 /**
@@ -46,7 +46,7 @@ function addJobsToDocument (jobs) {
   // Loop through each of the jobs in the passed in array,
   // and create a <job-details> element for each one
   for (const job of jobs) {
-    addJobToDocument(job);
+    addJobToDocument(job)
   }
 }
 
@@ -56,7 +56,7 @@ function addJobsToDocument (jobs) {
  * @param {Array<Object>} jobs An array of jobs
  */
 function saveJobsToStorage (jobs) {
-  localStorage.setItem('jobs', JSON.stringify(jobs));
+  localStorage.setItem('jobs', JSON.stringify(jobs))
 }
 
 /**
@@ -64,48 +64,48 @@ function saveJobsToStorage (jobs) {
  */
 function initFormHandler () {
   // Get a reference to the <form> element
-  const form = document.querySelector('form');
+  const form = document.querySelector('form')
   // Add an event listener for the 'submit' event,
   // which fires when the submit button is clicked
   form.addEventListener('submit', (event) => {
-    event.preventDefault();
+    event.preventDefault()
     // Create a new FormData object from the <form> element reference above
-    const formData = new FormData(form);
+    const formData = new FormData(form)
     // Create an empty object
-    const jobObject = {};
+    const jobObject = {}
     // Extract the keys and corresponding values from the
     // FormData object and insert them into jobObject
     for (const [key, value] of formData) {
-      jobObject[key] = value;
+      jobObject[key] = value
     }
     // Create or edit a <job-details> element with the data in jobObject
-    addJobToDocument(jobObject);
+    addJobToDocument(jobObject)
     // Reset the <job-details> element to edit
-    jobDetailsToEdit = null;
+    jobDetailsToEdit = null
     // Clear the <form> fields
-    clearForm();
+    clearForm()
 
     // TODO: code below can be shifted to addJobsToDocuments
 
     // Get the jobs array from localStorage
-    const jobs = getJobsFromStorage();
+    const jobs = getJobsFromStorage()
     // Add this new job to it
-    jobs.push(jobObject);
+    jobs.push(jobObject)
     // Save the jobs array back to localStorage
-    saveJobsToStorage(jobs);
+    saveJobsToStorage(jobs)
     // Hide the form
-    hideForm();
-  });
+    hideForm()
+  })
   // Add an event listener for when the cancel button is clicked
-  const cancelButton = form.querySelector('#cancel');
+  const cancelButton = form.querySelector('#cancel')
   cancelButton.addEventListener('click', () => {
     // Hide the form
-    hideForm();
+    hideForm()
     // Reset the <job-details> element to edit
-    jobDetailsToEdit = null;
+    jobDetailsToEdit = null
     // Clear the <form> fields
-    clearForm();
-  });
+    clearForm()
+  })
 }
 
 /**
@@ -113,13 +113,13 @@ function initFormHandler () {
  */
 function clearForm () {
   // Get a reference to the <form> element
-  const form = document.querySelector('form');
+  const form = document.querySelector('form')
   // Create a new FormData object from the <form> element reference above
-  const formData = new FormData(form);
+  const formData = new FormData(form)
   // For each key in the FormData object
   for (const key of formData.keys()) {
     // Clear the corresponding field in the form
-    form.querySelector(`#${key}`).value = null;
+    form.querySelector(`#${key}`).value = null
   }
 }
 
@@ -130,9 +130,9 @@ function clearForm () {
  */
 function addJobToDocument (job) {
   // Get a reference to the job-details-list element
-  const list = document.querySelector('#job-details-list');
+  const list = document.querySelector('#job-details-list')
   // Get the <job-details> element to edit, otherwise create a new <job-details> element
-  const jobDetails = jobDetailsToEdit || document.createElement("job-details");
+  const jobDetails = jobDetailsToEdit || document.createElement('job-details')
   /** TODO:
    * // generate new id
    * const new_id = new id();
@@ -150,48 +150,48 @@ function addJobToDocument (job) {
    * database.update(job);
    */
   // Add the job data to <job-details>
-  jobDetails.data = job;
+  jobDetails.data = job
   // Add the onClickDelete function to <job-details>
   jobDetails.onClickDelete = () => {
     // Get confirmation from user
-    showDeletePopup();
-    document.getElementById("delete-btn").addEventListener("click", deleteApp);
+    showDeletePopup()
+    document.getElementById('delete-btn').addEventListener('click', deleteApp)
     document
-      .getElementById("cancel-btn")
-      .addEventListener("click", cancelDeleteApp);
+      .getElementById('cancel-btn')
+      .addEventListener('click', cancelDeleteApp)
     // if (window.confirm('Are you sure you want to delete this?')) {
     //   // Remove the <job-details> element from job-details-list
     //   list.removeChild(jobDetails)
     // }
-  };
-
-  function deleteApp() {
-    // Remove the <job-details> element from job-details-list
-    list.removeChild(jobDetails);
-    hideDeletePopup();
   }
 
-  function cancelDeleteApp() {
-    hideDeletePopup();
+  function deleteApp () {
+    // Remove the <job-details> element from job-details-list
+    list.removeChild(jobDetails)
+    hideDeletePopup()
+  }
+
+  function cancelDeleteApp () {
+    hideDeletePopup()
   }
 
   // Get a reference to the <form> element
-  const form = document.querySelector("form");
+  const form = document.querySelector('form')
   // Add the onClickEdit function to <job-details>
   jobDetails.onClickEdit = () => {
     // Populate the fields in the <form> with the job data
     for (const property in job) {
-      form.querySelector(`#${property}`).value = job[property];
+      form.querySelector(`#${property}`).value = job[property]
     }
     // Set the <job-details> element to edit
-    jobDetailsToEdit = jobDetails;
+    jobDetailsToEdit = jobDetails
     // Show the form
-    showForm();
-  };
+    showForm()
+  }
   // If there is no <job-details> element to edit
   if (!jobDetailsToEdit) {
     // Append this new <job-details> to job-details-list
-    list.appendChild(jobDetails);
+    list.appendChild(jobDetails)
   }
 }
 
@@ -201,38 +201,38 @@ function addJobToDocument (job) {
  * @global
  * @type {JobDetails}
  */
-let jobDetailsToEdit = null;
+let jobDetailsToEdit = null
 
 /**
  * Hide the `form` element
  */
-function hideForm() {
+function hideForm () {
   document
-    .querySelector("#job-details-form")
-    .setAttribute("style", "display: none");
+    .querySelector('#job-details-form')
+    .setAttribute('style', 'display: none')
 }
 
 /**
  * Show the `form` element
  */
-function showForm() {
-  document.querySelector("#job-details-form").removeAttribute("style");
+function showForm () {
+  document.querySelector('#job-details-form').removeAttribute('style')
 }
 
 /**
  * Hide the `delete-popup` element
  */
-function hideDeletePopup() {
+function hideDeletePopup () {
   document
-    .querySelector("#delete-popup-container")
-    .setAttribute("style", "display: none");
+    .querySelector('#delete-popup-container')
+    .setAttribute('style', 'display: none')
 }
 
 /**
  * Show the `delete-popup` element
  */
-function showDeletePopup() {
-  document.querySelector("#delete-popup-container").removeAttribute("style");
+function showDeletePopup () {
+  document.querySelector('#delete-popup-container').removeAttribute('style')
 }
 
 /**
@@ -242,19 +242,19 @@ function showDeletePopup() {
  * to `sort-bar-list`.
  * @param {Array<Object>} fieldNames An array of field names
  */
-function addSortBars(fieldNames) {
-  const sortBarList = document.querySelector("#sort-bar-list");
+function addSortBars (fieldNames) {
+  const sortBarList = document.querySelector('#sort-bar-list')
   for (const fieldName of fieldNames) {
-    const sortBar = document.createElement("sort-bar");
-    sortBar.fieldName = fieldName;
+    const sortBar = document.createElement('sort-bar')
+    sortBar.fieldName = fieldName
     sortBar.onClick = () => {
       if (sortBar.fieldEnabled) {
-        setSortRule(fieldName, sortBar.orderReversed);
+        setSortRule(fieldName, sortBar.orderReversed)
       } else {
-        removeSortRule(fieldName);
+        removeSortRule(fieldName)
       }
-    };
-    sortBarList.appendChild(sortBar);
+    }
+    sortBarList.appendChild(sortBar)
   }
 }
 
@@ -265,7 +265,7 @@ function addSortBars(fieldNames) {
  * @global
  * @type {Object}
  */
-const sortRules = {};
+const sortRules = {}
 
 /**
  * Set a sorting rule in `sortRules`.
@@ -273,9 +273,9 @@ const sortRules = {};
  * @param {String} fieldName The name of a field to sort by
  * @param {Boolean} ascOrDesc Whether to sort in ascending or descending order
  */
-function setSortRule(fieldName, ascOrDesc) {
-  sortRules[fieldName] = ascOrDesc;
-  onSortRulesChanged();
+function setSortRule (fieldName, ascOrDesc) {
+  sortRules[fieldName] = ascOrDesc
+  onSortRulesChanged()
 }
 
 /**
@@ -283,14 +283,14 @@ function setSortRule(fieldName, ascOrDesc) {
  *
  * @param {String} fieldName The name of a field to no longer sort by
  */
-function removeSortRule(fieldName) {
-  delete sortRules[fieldName];
-  onSortRulesChanged();
+function removeSortRule (fieldName) {
+  delete sortRules[fieldName]
+  onSortRulesChanged()
 }
 
 /**
  * Called when `sortRules` is changed.
  */
-function onSortRulesChanged() {
-  console.log(sortRules);
+function onSortRulesChanged () {
+  console.log(sortRules)
 }
